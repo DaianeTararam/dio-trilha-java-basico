1d62bd36619406012ac20bdb5879cecd6e3c1682
DaianeTararam <daalkmim@gmail.com>
2023-03-19 19:20:14 -0300

    Projeto de Estudo JAVA

--- tipos-variaveis/src/TerminalArgumentos/MinhaClasse.java
+++ tipos-variaveis/src/TerminalArgumentos/MinhaClasse.java
@@ -2,6 +2,7 @@ package TerminalArgumentos;
 
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo terminal");
+        System.out.println("Oi, fui executado pelo terminal..");
+
     }
 }
